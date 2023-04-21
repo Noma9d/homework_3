c89e0f09ef39aecb97e1d2690ddd95758989ee8f
Noma9d <woody0740@gmail.com>
2023-04-21 21:15:36 +0300

    modified:   birthday.py

--- birthday.py
+++ birthday.py
@@ -21,11 +21,12 @@ def get_birthdays_per_week(users):
     }
 
     today = datetime.now()
-    end_day = today + timedelta(days = 6)
+    start_of_weekday = today - timedelta(days = today.weekday() + 2)
+    end_of_weekday = start_of_weekday + timedelta(days = 6)
     for item in users:
         day = item['birthday']
-        if today.month == day.month and today.day <= day.day <= end_day.day:
-            if day.weekday() in (5, 6):
+        if today.month == day.month and start_of_weekday.day <= day.day <= end_of_weekday.day:
+            if day.weekday() in (5, 6, 0):
                 result['Monday'].append(item['name'])
             else:
                 result[week_days[day.weekday()]].append(item['name'])
@@ -42,9 +43,14 @@ def get_birthdays_per_week(users):
 
 print(get_birthdays_per_week([
                             {'name':'Ivan', 'birthday':datetime(year=1995, month=4, day=20)}, #Thursday
-                            {'name':'Ivan', 'birthday':datetime(year=1995, month=4, day=22)}, #Saturday
-                            {'name':'Alex', 'birthday':datetime(year=1996, month=5, day=18)}, #Saturday
-                            {'name':'Dima', 'birthday':datetime(year=1996, month=4, day=24)}, #Wednesday
+                            {'name':'Oleg', 'birthday':datetime(year=1995, month=4, day=16)}, #Sunday
+                            {'name':'Alex', 'birthday':datetime(year=1996, month=4, day=17)}, #Wednesday
+                            {'name':'Dima', 'birthday':datetime(year=1996, month=4, day=15)}, #Monday
+                            {'name':'Julia', 'birthday':datetime(year=1996, month=4, day=16)}, #Tuesday
+                            {'name':'Irina', 'birthday':datetime(year=1996, month=4, day=19)}, #Friday
                             {'name':'Vasya', 'birthday':datetime(year=1996, month=4, day=25)}, #Thursday
                             {'name':'Petro', 'birthday':datetime(year=1998, month=4, day=25)} #Saturday
                             ]))
+
+# a = datetime(year=1996, month=4, day=19)
+# print(a.weekday())
